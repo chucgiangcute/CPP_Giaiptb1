661d1492279a5cf5320deac579779d527b292a87
Giang Chúc <chucgiang218@gmail.com>
2024-06-24 19:41:00 +0700

    Sửa lại bài

--- CPP_Giaiptb1.cpp
+++ CPP_Giaiptb1.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	double a, b, x;
-	cout << "Nhap gia tri a";
-	cin >> a;
-	cout << "Nhap gia tri b";
-	cin >> b;
-	if (a == 0) {
-		cout << "Gia tri x la :";
-	}
-	x = -b / a;
-	cout << "Gia tri x la :" << x;
-	return 0;
+    double a, b, x;
+    cout << "nhap a";
+    cin >> a;
+    cout << "nhap b";
+    cin >> b;
+    x = -b / a;
+    if (a == 0 && b == 0) {
+        cout << "phuong trinh dung voi bac x";
+    }
+    else if (a == 0 && b != 0) {
+        cout << "vo nghiem";
+    }
+    else if (a != 0) {
+        cout << "nghiem" << x;
+    }
+    return 0;
 }
